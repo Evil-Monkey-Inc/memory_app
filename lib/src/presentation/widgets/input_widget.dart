b19f6e3a31a759f2b31817f020b9b5bc77b36d24
Sashulya00 <zavalieblodaun@gmail.com>
2023-06-04 05:17:12 +0300

    create widget for TextFormFields

---
+++ lib/src/presentation/widgets/input_widget.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class InputWidget extends StatefulWidget {
+  InputWidget({super.key, required this.labelText, this.maxLines, this.onChanged, this.titleController});
+  final String labelText;
+  final int? maxLines;
+  final Function (String)? onChanged;
+  final TextEditingController? titleController;
+
+  @override
+  State<InputWidget> createState() => _InputWidgetState();
+}
+
+class _InputWidgetState extends State<InputWidget> {
+  final _titleFocusNode = FocusNode();
+
+
+  @override
+  void dispose() {
+    _titleFocusNode.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      onChanged: widget.onChanged,
+      controller: widget.titleController,
+      focusNode: _titleFocusNode,
+      maxLines: widget.maxLines,
+      decoration: InputDecoration(
+        labelText: widget.labelText,
+        alignLabelWithHint: true,
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+      ),
+    );
+  }
+}
